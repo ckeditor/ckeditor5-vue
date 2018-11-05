1a684beb2a806c604d2f07c5e1294d8d430baee2
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2018-11-05 17:55:58 +0100

    Internal: Build.

--- dist/ckeditor.js
+++ dist/ckeditor.js
@@ -249,16 +249,9 @@ const CKEditor = {
   * Instals the plugin, registering the `<ckeditor>` component.
   *
   * @param {Vue} Vue The Vue object.
-  * @param {Object} [config] Plugin configuration.
-  * @param {Object.<String,Function>} [config.editors] The configuration of editor constructors and their names.
   */
-	install(Vue, config) {
-		// https://github.com/gotwarlost/istanbul/issues/665
-		config = config || {};
-
+	install(Vue) {
 		Vue.component('ckeditor', _ckeditor_js__WEBPACK_IMPORTED_MODULE_0__["default"]);
-
-		Vue.prototype.$_ckeditor_types = config.editors;
 	},
 	component: _ckeditor_js__WEBPACK_IMPORTED_MODULE_0__["default"]
 };
