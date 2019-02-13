cb9cb483157f8e2acef131715780d329f2ff4051
Kamil Piechaczek <pomek@pomek.pl>
2019-02-13 11:49:42 +0100

    Internal: Bumped the year.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
+/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
 // Note: The ESLint configuration is mandatory for vue-cli.
 module.exports = {
     "extends": "ckeditor5"
