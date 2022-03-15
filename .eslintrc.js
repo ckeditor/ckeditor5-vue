546ca71b96e3cedc09992a7c34705726e0a9753c
Przemyslaw Zan <p.zan@cksource.com>
2022-03-15 18:03:12 +0100

    Allowed the "license-header" ESLint rule. Fixed broken license headers across the project.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,17 @@
  * For licensing, see LICENSE.md.
  */
 
+/* global module */
+
 // Note: The ESLint configuration is mandatory for vue-cli.
 module.exports = {
-	"extends": "ckeditor5"
+	'extends': 'ckeditor5',
+	'rules': {
+		'ckeditor5-rules/license-header': [ 'error', { headerLines: [
+			'/**',
+			' * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.',
+			' * For licensing, see LICENSE.md.',
+			' */'
+		] } ]
+	}
 };
