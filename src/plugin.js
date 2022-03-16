8f57c3e6b547c44113431b1ddf86d2f50f20d32e
Paweł Smyrek <p.smyrek@cksource.com>
2022-03-16 11:05:28 +0100

    Reverted unnecessary changes

--- src/plugin.js
+++ src/plugin.js
@@ -3,11 +3,12 @@
  * For licensing, see LICENSE.md.
  */
 
-import * as Vue from 'vue';
+import Vue, { version as vueVersion } from 'vue';
 import CKEditorComponent from './ckeditor.js';
 
 /* istanbul ignore next */
-const [ major ] = Vue.version.split( '.' ).map( i => parseInt( i, 10 ) );
+const version = Vue ? Vue.version : vueVersion;
+const [ major ] = version.split( '.' ).map( i => parseInt( i, 10 ) );
 
 /* istanbul ignore if */
 if ( major < 3 ) {
