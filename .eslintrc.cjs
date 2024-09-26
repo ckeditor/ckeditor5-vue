eb91b1b3d2b8a8de0ce37091d13e1d80502865ac
Filip Sobol <filipsobol@users.noreply.github.com>
2024-09-26 10:24:41 +0200

    Fix formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,8 +23,7 @@ module.exports = {
 			' * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.',
 			' * For licensing, see LICENSE.md.',
 			' */'
-		]
-		} ],
+		] } ],
 		'ckeditor5-rules/require-file-extensions-in-imports': [
 			'error',
 			{
