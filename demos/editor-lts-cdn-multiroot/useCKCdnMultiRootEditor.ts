05186423d7415992f25dedd3b592bcbd3b24f7dc
Filip Sobol <filipsobol@users.noreply.github.com>
2026-06-18 13:50:29 +0200

    Add demo for LTS editor with multiroot support.

---
+++ demos/editor-lts-cdn-multiroot/useCKCdnMultiRootEditor.ts
@@ -0,0 +1,103 @@
+/**
+ * @license Copyright (c) 2003-2026, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-licensing-options
+ */
+
+import type { MultiRootEditor as MultiRootEditorType } from 'https://cdn.ckeditor.com/typings/ckeditor5.d.ts';
+import type { CKEditorCloudConfig, CKEditorCloudResult } from '../../src/plugin.js';
+
+export function createCKCdnMultiRootEditor(
+	cloud: CKEditorCloudResult<CKEditorCloudConfig>
+): typeof MultiRootEditorType {
+	const {
+		MultiRootEditor: MultiRootEditorBase,
+		CloudServices,
+		Essentials,
+		CKFinderUploadAdapter,
+		Autoformat,
+		Bold,
+		Italic,
+		BlockQuote,
+		CKBox,
+		CKFinder,
+		EasyImage,
+		Heading,
+		Image,
+		ImageCaption,
+		ImageStyle,
+		ImageToolbar,
+		ImageUpload,
+		Indent,
+		Link,
+		List,
+		MediaEmbed,
+		Paragraph,
+		PasteFromOffice,
+		PictureEditing,
+		Table,
+		TableToolbar,
+		TextTransformation
+	} = cloud.CKEditor;
+
+	return class MultiRootEditor extends MultiRootEditorBase {
+		public static override builtinPlugins = [
+			Essentials,
+			CKFinderUploadAdapter,
+			Autoformat,
+			Bold,
+			Italic,
+			BlockQuote,
+			CKBox,
+			CKFinder,
+			CloudServices,
+			EasyImage,
+			Heading,
+			Image,
+			ImageCaption,
+			ImageStyle,
+			ImageToolbar,
+			ImageUpload,
+			Indent,
+			Link,
+			List,
+			MediaEmbed,
+			Paragraph,
+			PasteFromOffice,
+			PictureEditing,
+			Table,
+			TableToolbar,
+			TextTransformation
+		];
+
+		public static override defaultConfig = {
+			licenseKey: '<YOUR_LICENSE_KEY>',
+			toolbar: {
+				items: [
+					'undo', 'redo',
+					'|', 'heading',
+					'|', 'bold', 'italic',
+					'|', 'link', 'uploadImage', 'insertTable', 'blockQuote', 'mediaEmbed',
+					'|', 'bulletedList', 'numberedList', 'outdent', 'indent'
+				]
+			},
+			image: {
+				toolbar: [
+					'imageStyle:inline',
+					'imageStyle:block',
+					'imageStyle:side',
+					'|',
+					'toggleImageCaption',
+					'imageTextAlternative'
+				]
+			},
+			table: {
+				contentToolbar: [
+					'tableColumn',
+					'tableRow',
+					'mergeTableCells'
+				]
+			},
+			language: 'en'
+		};
+	};
+}
